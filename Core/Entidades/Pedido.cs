d0c3779853cd1372f147d742356270db8e3d125f
Carlos Aragão <professor.carlosaragao@gmail.com>
2023-07-15 14:31:55 -0300

    first commit

---
+++ Core/Entidades/Pedido.cs
@@ -0,0 +1,21 @@
+﻿namespace Core.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public Usuario Usuario { get; set; }
+        public DateTime DataCriacao { get; set; }
+
+        public Pedido(int id, Usuario usuario)
+        {
+            Id = id;
+            Usuario = usuario;
+            DataCriacao = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"Pedido Id {Id}, Usuário {Usuario.Nome}, Data de criação: {DataCriacao:dd/MM/yyyy}";
+        }
+    }
+}
---
+++ Core/Entidades/Usuario.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Usuario(int id, string nome, string email)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+        }
+    }
+}
